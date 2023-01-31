e52a95b2ba9391855c12affed7cd7cef711b8aed
AmanCoderPro <87359434+AmanCoderPro@users.noreply.github.com>
2023-01-31 20:26:44 +0800

    Add files via upload

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
 status = "";
+objects = [];
 
 function setup() {
     canvas = createCanvas(480, 380);
     canvas.center();
     video = createCapture(VIDEO);
+    video.size(480, 380);
     video.hide();
 }
 
@@ -20,4 +22,36 @@ function modelLoaded() {
 
 function draw() {
     image(video, 0, 0, 480, 380);
+    if(status != "") {
+        objectDetector.detect(video, gotResult);
+
+        for(i = 0; i < objects.length; i++) {
+            document.getElementById('status').innerHTML = "Status : Objects Detected";
+            percent = floor(objects[i].confidence * 100);
+            noFill();
+            text(objects[i].label + "" + percent + "%", objects[i].x + 15, objects[i].y + 15);
+            stroke("#1286E0");
+            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+
+            if(objects[i].label == object_name) {
+                video.stop();
+                objectDetector.detect(gotResult);
+                document.getElementById("object_found").innerHTML = object_name + " Found";
+                synth = window.speechSynthesis;
+                utterThis = new SpeechSynthesisUtterance(object_name + "found");
+                synth.speak(utterThis);
+            }
+            else {
+                document.getElementById("object_found").innerHTML = object_name + " Not Found";
+            }
+        }
+    }
+}
+
+function gotResult(error, results) {
+    if(error) {
+        console.error(error);
+    }
+    console.log(results);
+    objects = results;
 }
